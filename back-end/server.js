326124123d261686d672dd0f57a3cf319eea3e53
tesessa <tessaeliseandersen@yahoo.com>
2022-03-29 04:57:15 +0000

    Done?

--- back-end/server.js
+++ back-end/server.js
@@ -15,4 +15,102 @@ mongoose.connect('mongodb://localhost:27017/museum', {
 });
 
 
+
+// Configure multer so that it will upload to '../front-end/public/images'
+const multer = require('multer')
+const upload = multer({
+  dest: '../front-end/public/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
+
+// Create a scheme for items in the museum: a title and a path to an image.
+const itemSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  path: String,
+});
+
+// Create a model for items in the museum.
+const Item = mongoose.model('Item', itemSchema);
+
+//Delete items
+app.delete('/api/items/:id', async (req, res) => {
+console.log("Delete", req.params.id);
+try {
+    await Item.deleteOne();
+	_id: req.params.id
+	res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+
+app.put('/api/items/:id', async (req, res) => {
+let newTitle = req.body.title;
+try {
+	let newTitle = req.body.title;
+	let newDesc = req.body.description;
+	const item = await Item.findOne({
+		_id: req.params.id
+	})
+	item.title = newTitle;
+	item.description = newDesc;
+	item.save();
+	res.sendStatus(200);
+
+    } catch (error) {
+	    console.log(error);
+	    res.sendStatus(500);
+   }
+});
+
+
+// Get a list of all of the items in the museum.
+app.get('/api/items', async (req, res) => {
+try {
+    let items = await Item.find();
+    res.send(items);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+
+// Upload a photo. Uses the multer middleware for the upload and then returns
+// the path where the photo is stored in the file system.
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+  // Just a safety check
+  console.log("Get photos");
+  console.log("api/photos ", req.file);
+  if (!req.file) {
+    return res.sendStatus(400);
+  }
+  res.send({
+    path: "/images/" + req.file.filename
+  });
+});
+
+// Create a new item in the museum: takes a title and a path to an image.
+app.post('/api/items', async (req, res) => {
+   console.log("api/items ", req.body.title);
+   const item = new Item({
+    title: req.body.title,
+    description: req.body.description,
+    path: req.body.path,
+  });
+  try {
+    await item.save();
+    res.send(item);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+
 app.listen(3000, () => console.log('Server listening on port 3000!'));
